98dfc2b0864b7ea16096ddcf6ca2f03c37b567e3
Javier Hidalgo <javichitone@gmail.com>
2013-04-28 13:17:47 -0500

    Solve first task from kata-1

---
+++ tdd-kata-1/README.md
@@ -0,0 +1,9 @@
+# String Calculator
+
+Implements the first kata from [this website](http://osherove.com/tdd-kata-1/) using Minitest.
+
+## Run code
+
+You need at least Ruby 1.9
+
+    ruby string_calculator_test.rb
---
+++ tdd-kata-1/string_calculator_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative 'string_calculator'
+
+class StringCalculatorTest < MiniTest::Unit::TestCase
+  def test_add_with_empty_string
+    assert_equal 0, StringCalculator.add("")
+  end
+
+  def test_add_with_one_number
+    assert_equal 5, StringCalculator.add("5")
+  end
+
+  def test_add_with_two_numbers
+    assert_equal 12, StringCalculator.add("5,7")
+  end
+end
